bd2eec636eb0dbcac26438592d10cc84ead129e4
r3yesmauricio <maury99900@gmail.com>
2023-05-20 16:47:00 -0600

    Crud de usuarios

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use DB;
 use Hash;
+use Validator;
 class UsersController extends Controller
 {
     /**
@@ -45,4 +46,94 @@ class UsersController extends Controller
 
         return $usuarios;
     }
+
+    public function store(Request $request)
+    {
+        $inputs = $request->all();
+
+        $validator = Validator::make($inputs, [
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                "status"=> false,
+                "errors"=>$validator->errors()
+            ];
+
+            return response()->json($response);
+        }
+        $usuario = new User();
+        $usuario->name=$request->get('name');
+        $usuario->email=$request->get('email');
+        $usuario->password=Hash::make($request->get('password'));
+        $usuario->id_empleado=$request->get('id_empleado');
+        $usuario->is_admin=($request->get('admin') == 1) ? 1 : 0;
+
+        $usuario->save();
+
+        $response = [
+            "status"=>true,
+            "message"=>"Datos insertados correctamente",
+            'data' => $usuario
+        ];
+        return response()->json($response);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+
+        //validacion de entradas
+        $validator = Validator::make($inputs, [
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                "status"=> false,
+                "errors"=>$validator->errors()
+            ];
+
+            return response()->json($response);
+        }
+
+        $user = User::find($id);
+        $user->name=$request->get('name');
+        $user->email=$request->get('email');
+        $user->password=Hash::make($request->get('password'));
+        $user->id_empleado=$request->get('id_empleado');
+        $user->is_admin=($request->get('admin') == 1) ? 1 : 0;
+        
+        $user->update();
+
+        $response = [
+            "status"=>true,
+            "message"=>"Datos actualizados correctamente",
+            'data' => $user
+        ];
+
+        return response()->json($response);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        $user->status = false;
+
+        $user->update();
+
+        $response = [
+            "status"=> true,
+            "data"=>$user,
+            "message"=>"El registro fue eliminado correctamente"
+        ];
+
+        return response()->json($response);
+    }
+
 }
